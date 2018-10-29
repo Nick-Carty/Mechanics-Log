f9403049c9173cadcec1a549a08a17c47fd620c9
Nicholas Carty <nickcarty13@gmail.com>
2018-10-29 16:00:48 -0400

    Added to the user table and changed ruby version

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :user_name, presence: true, uniqueness: true
+
+  def admin?
+    role == "admin"
+  end
+
+  def authorize_user
+    if !user_signed_in || !current_user.admin?
+      flash[:notice] = "You do not have access to this page."
+      redirect_to root_path
+    end
+  end
 end
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -3,8 +3,12 @@ require 'factory_bot'
 FactoryBot.define do
   factory :user do
     sequence(:email) {|n| "user#{n}@example.com" }
+    first_name { 'Charles' }
+    last_name { 'Xavier' }
+    user_name { 'Xman1' }
     password { 'password' }
     password_confirmation { 'password' }
+    role { 'member' }
   end
 
 end
